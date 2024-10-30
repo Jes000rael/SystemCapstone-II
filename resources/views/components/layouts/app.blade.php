b43bb3ebd0636a1b96dc27cfe5fbca859ec2c7e2
Jes000rael <jes2kim6@gmail.com>
2024-10-30 10:36:08 +0800

    Attendance_v2.0

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,9 +1,21 @@
 <x-layouts.base>
-
+@auth
 
 @include('components.layouts.navbars.sidebar')
 @include('components.layouts.navbars.topbar')
 {{ $slot }}
+@endauth
+
+@guest
+        {{-- If the user is on the login page --}}
+        @if (!auth()->check() && in_array(request()->route()->getName(),['login'],))
+           
+            {{ $slot }}
+          
+
+       
+        @endif
+    @endguest
 
 
 
