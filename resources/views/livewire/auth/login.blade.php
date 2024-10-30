f3b6019d1650b4d934ee67fcbaf30ade3b33077d
Jes000rael <jes2kim6@gmail.com>
2024-10-30 11:33:33 +0800

    Attendance_v2.0

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,40 +1,85 @@
-<div>
-       <div class="account-pages my-5 pt-sm-5">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-8 col-lg-6 col-xl-5">
-                        <div class="card overflow-hidden">
-                            <div class="bg-primary bg-soft">
+
+@push('styles')
+<style>
+    .password-container {
+    position: relative;
+    
+}
+
+input[type="password"],
+input[type="text"] {
+    width: 100%;
+    padding: 10px;
+    padding-right: 40px; /* Space for the icon */
+    box-sizing: border-box;
+}
+
+.toggle-password {
+    position: absolute;
+    top: 50%;
+    right: 10px;
+    transform: translateY(-50%);
+    cursor: pointer;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+
+
+.background-image {
+    background-image: url('https://preview.redd.it/snow-capped-4k-3840x2160-by-a-i-v0-npeh96ogvkea1.jpg?width=1080&crop=smart&auto=webp&s=a29fc17ba5671e1788319a763ed492f1c2afa2cf'); /* Replace with your image URL */
+    background-size: cover; /* Cover the entire div */
+    background-position: center; /* Center the image */
+    height: 100vh; /* Full height of the viewport */
+    width: 100%; /* Full width */
+    display: flex; /* Center content */
+    justify-content: center; /* Center horizontally */
+    align-items: center; /* Center vertically */
+  
+    
+}
+
+
+.transparent-card {
+        background-color: rgba(255, 255, 255, 0.5); /* White background with 50% opacity */
+        border: none; /* Remove default border */
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.2); /* Optional: Add a subtle shadow */
+        backdrop-filter: blur(5px); /* Optional: Apply a blur effect to the background */
+    }
+
+    .card-body {
+        color: #000; /* Set text color for better visibility */
+    }
+</style>
+
+
+@endpush
+
+<div class="background-image">
+       <div class="account-pages  pt-sm-4 ">
+            <div class="container ">
+                <div class="row justify-content-center ">
+                    <div class="col-md-8 col-lg-6 col-xl-5 ">
+                        <div class="card overflow-hidden transparent-card ">
+                            <div >
                                 <div class="row">
-                                    <div class="col-7">
+                                    <!-- <div class="col-7">
                                         <div class="text-primary p-4">
                                             <h5 class="text-primary">Welcome Back !</h5>
                                             <p>Sign in to continue to Skote.</p>
                                         </div>
-                                    </div>
-                                    <div class="col-5 align-self-end">
-                                        <img src="assets/images/profile-img.png" alt="" class="img-fluid">
+                                    </div> -->
+                                    <div class="col-12 align-self-end py-4 px-4">
+                                        <img  src="https://cdn.shopify.com/s/files/1/0636/4479/5108/files/Prime_Retail_fall_logo.png?v=1727795811" alt="" class="img-fluid">
                                     </div>
                                 </div>
                             </div>
                             <div class="card-body pt-0"> 
-                                <div class="auth-logo">
-                                    <a href="index.html" class="auth-logo-light">
-                                        <div class="avatar-md profile-user-wid mb-4">
-                                            <span class="avatar-title rounded-circle bg-light">
-                                                <img src="assets/images/logo-light.svg" alt="" class="rounded-circle" height="34">
-                                            </span>
-                                        </div>
-                                    </a>
-
-                                    <a href="index.html" class="auth-logo-dark">
-                                        <div class="avatar-md profile-user-wid mb-4">
-                                            <span class="avatar-title rounded-circle bg-light">
-                                                <img src="assets/images/logo.svg" alt="" class="rounded-circle" height="34">
-                                            </span>
-                                        </div>
-                                    </a>
-                                </div>
+                               
                                 <div class="p-2">
                                     <form class="form-horizontal" action="index.html">
         
@@ -45,63 +90,59 @@
                 
                                         <div class="mb-3">
                                             <label class="form-label">Password</label>
-                                            <div class="input-group auth-pass-inputgroup">
-                                                <input type="password" class="form-control" placeholder="Enter password" aria-label="Password" aria-describedby="password-addon">
-                                                <button class="btn btn-light " type="button" id="password-addon"><i class="mdi mdi-eye-outline"></i></button>
+                                            <div class="password-container">
+                                                <input id="password" type="password" class="form-control" placeholder="Enter password" aria-label="Password" aria-describedby="password-addon">
+                                                <span id="togglePassword" class="toggle-password" onclick="togglePassword()">
+            <i class="fas fa-eye"></i>
+        </span>
                                             </div>
                                         </div>
 
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" id="remember-check">
-                                            <label class="form-check-label" for="remember-check">
-                                                Remember me
-                                            </label>
-                                        </div>
+                                       
                                         
-                                        <div class="mt-3 d-grid">
-                                            <button class="btn btn-primary waves-effect waves-light" type="submit">Log In</button>
-                                        </div>
-            
-                                        <div class="mt-4 text-center">
-                                            <h5 class="font-size-14 mb-3">Sign in with</h5>
-            
-                                            <ul class="list-inline">
-                                                <li class="list-inline-item">
-                                                    <a href="javascript::void()" class="social-list-item bg-primary text-white border-primary">
-                                                        <i class="mdi mdi-facebook"></i>
-                                                    </a>
-                                                </li>
-                                                <li class="list-inline-item">
-                                                    <a href="javascript::void()" class="social-list-item bg-info text-white border-info">
-                                                        <i class="mdi mdi-twitter"></i>
-                                                    </a>
-                                                </li>
-                                                <li class="list-inline-item">
-                                                    <a href="javascript::void()" class="social-list-item bg-danger text-white border-danger">
-                                                        <i class="mdi mdi-google"></i>
-                                                    </a>
-                                                </li>
-                                            </ul>
+                                        <div class="mt-3 d-grid ">
+                                            <button class="btn btn-primary waves-effect waves-light rounded-pill" type="submit">Log In</button>
                                         </div>
 
-                                        <div class="mt-4 text-center">
-                                            <a href="auth-recoverpw.html" class="text-muted"><i class="mdi mdi-lock me-1"></i> Forgot your password?</a>
-                                        </div>
+                                        <div class="mt-5 text-center">
+                            
+                            
+                                
+                                <p>© <script>document.write(new Date().getFullYear())</script> Design & Develop <i class="mdi mdi-heart text-danger"></i> by Enopoly Team </p>
+                           
+                        </div>
+            
+                                        
                                     </form>
                                 </div>
             
                             </div>
                         </div>
-                        <div class="mt-5 text-center">
-                            
-                            <div>
-                                <p>Don't have an account ? <a href="auth-register.html" class="fw-medium text-primary"> Signup now </a> </p>
-                                <p>© <script>document.write(new Date().getFullYear())</script> Skote. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand</p>
-                            </div>
-                        </div>
+                       
 
                     </div>
                 </div>
             </div>
         </div>
 </div>
+
+@push('scripts')
+<script>
+    function togglePassword() {
+    const passwordInput = document.getElementById('password');
+    const toggleIcon = document.getElementById('togglePassword').querySelector('i');
+    
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        toggleIcon.classList.remove('fa-eye');
+        toggleIcon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        toggleIcon.classList.remove('fa-eye-slash');
+        toggleIcon.classList.add('fa-eye');
+    }
+}
+
+</script>
+@endpush
+
