9a9df1b55fc7f9ff609b79db0b7f269b70c90f08
Jes000rael <jes2kim6@gmail.com>
2024-11-05 09:22:13 +0800

    Attendance_v2.0

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,20 @@ use Illuminate\Support\Facades\Route;
 
 use App\Livewire\Auth\Login;
 use App\Livewire\Dashboard;
+use App\Livewire\EmployeeRecords;
 
 use Illuminate\Http\Request;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 Route::get('/', function() {
     return redirect('/login');
 });
-
 Route::get('/login', Login::class)->name('login');
-Route::get('/dashboard', Dashboard::class)->name('dashboard');
+
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/dashboard', Dashboard::class)->name('dashboard');
+    Route::get('/employee', EmployeeRecords::class)->name('employee-records');
+    
+});
